9e9c97f56797dc2e9ddecb1c16e94e24361b2750
Nephtaly Aniceta <iiikkkkkkeee@hotmail.com>
2017-03-28 08:14:50 +0200

    edited icons

--- Lola/Lola/TestDialog.cpp
+++ Lola/Lola/TestDialog.cpp
@@ -4,7 +4,8 @@ TestDialog::TestDialog(QWidget *parent)
 	: QDialog(parent)
 {
 	setupUi(this);
-	this->setFixedSize(QSize(1280, 720));
+	this->setFixedSize(QSize(800, 480));
+	MENU->setFocus();
 }
 
 TestDialog::~TestDialog()
@@ -17,19 +18,133 @@ bool TestDialog::event(QEvent *event)
 	{
 		QKeyEvent *ke = static_cast<QKeyEvent *>(event);
 		int cmd = ke->key();
-		switch(cmd)
+		if(!stackSlide->inAnimation())
 		{
-			case Qt::Key_4:
-				stackSlide->slideInPrev();
-				break;
-			case Qt::Key_6:
-				stackSlide->slideInNext();
-				break;
-			case Qt::Key_5:
-				stackSlide->slideHome();
-				break;
-			default:
-				break;
+			if(cmd >= Qt::Key_0 && cmd <= Qt::Key_9) //filter for numeric-keys
+			{
+				int value;
+				int itemNumbers = MENU->itemNumbers();
+
+				//Event handler for qtPieMenu
+				if(MENU->hasFocus())
+				{
+					switch(cmd)
+					{
+						case Qt::Key_4:
+						{
+							value = MENU->selection() - 1;
+							break;
+						}
+						case Qt::Key_6:
+						{
+							value = MENU->selection() + 1;
+							break;
+						}
+						case Qt::Key_5:
+						{
+							MENU->select();
+
+							break;
+						}
+						default:
+							break;
+					}
+
+					//Only change if the value is changed
+					if(cmd == Qt::Key_4 || cmd == Qt::Key_6)
+					{
+						if(value > itemNumbers - 1)
+							value = (value) % itemNumbers;
+						else if(value < 0)
+							value = (value + itemNumbers) % itemNumbers;
+
+						MENU->setSelection(value);
+					}
+				}
+				//Dail event handler
+				else if(false) //volumeDial->hasFocus()
+				{
+					QDial* dial = (QDial*)focusWidget();
+					if(dial->notchesVisible())
+					{
+						switch(cmd)
+						{
+							case Qt::Key_7:
+							case Qt::Key_5:
+								dial->setNotchesVisible(false);
+								break;
+							case Qt::Key_8:
+							case Qt::Key_6:
+							{
+								dial->setValue(dial->value() + 1);
+								break;
+							}
+							case Qt::Key_2:
+							case Qt::Key_4:
+							{
+								dial->setValue(dial->value() - 1);
+								break;
+							}
+							default:
+								break;
+						}
+					}
+					else
+					{
+						switch(cmd)
+						{
+							case Qt::Key_5:
+								dial->setNotchesVisible(true);
+								break;
+							case Qt::Key_8:
+							case Qt::Key_4:
+							{
+								focusPreviousChild();
+								break;
+							}
+							case Qt::Key_2:
+							case Qt::Key_6:
+							{
+								focusNextChild();
+								break;
+							}
+							case Qt::Key_7:
+								stackSlide->slideHome();
+								MENU->setFocus();
+								break;
+							default:
+								break;
+						}
+					}
+				}
+				else
+				{
+					switch(cmd)
+					{
+						case Qt::Key_8:
+						case Qt::Key_4:
+						{
+							focusPreviousChild();
+							break;
+						}
+						case Qt::Key_2:
+						case Qt::Key_6:
+						{
+							focusNextChild();
+							break;
+						}
+						default:
+							break;
+					}
+				}
+			}
+		}
+
+		//Always return to home and set focus
+		if(ke->key() == Qt::Key_0)
+		{
+			stackSlide->slideHome();
+			MENU->setFocus();
 		}
 		return true;
 	}
